2194c3df2d084a0e7df85d1537910e14cb05654c
Cw020729 <lchowwai@gmail.com>
2023-12-15 17:33:04 +0800

    Added data validation for user register

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -13,22 +13,60 @@ export class SignupComponent {
     email: '',
     hashedpassword: '',
   };
+  passwordInputFocused = false;
   
 
   constructor(private userService: UserService, private router: Router) {}
 
   registerUser() {
     // Call the user service to register the user
-    this.userService.registerUser(this.user).subscribe(
-      (response) => {
-        console.log('User registered successfully:', response);
-        // Optionally, navigate to another page after successful registration
-        this.router.navigate(['/login']);
-      },
-      (error) => {
-        console.error('Registration failed:', error);
-        // Handle registration failure (show error message, etc.)
-      }
-    );
+      if (this.isFormDataValid()){
+      this.userService.registerUser(this.user).subscribe(
+        (response) => {
+          console.log('User registered successfully:', response);
+          alert("You have successfully registered")
+          // Optionally, navigate to another page after successful registration
+          this.router.navigate(['/login']);
+        },
+        (error) => {
+          console.error('Registration failed:', error);
+          // Handle registration failure (show error message, etc.)
+        }
+      );
+    }
   }
+
+  isFormDataValid(): boolean {
+    const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{3,15}$/;
+    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    if (!this.user.username.trim()) {
+      alert('Please enter your Name');
+      return false;
+    }else if (!/^[a-zA-Z0-9 ]+$/.test(this.user.username.trim())){
+      alert('Item Name should only contain letter and numbers') ;
+      return false;
+    }
+
+    if (!this.user.email.trim()) {
+      alert('Please enter your email');
+      return false;
+    }else if(!emailRegex.test(this.user.email)){
+      alert('Please enter an valid email');
+      return false
+    }
+
+
+    if (!this.user.hashedpassword.trim()) {
+      alert('Please enter your password');
+      return false;
+    }else if (!passwordRegex.test(this.user.hashedpassword.trim())){
+      alert('Password should contain 3-15 characters with a combination of letters, numbers, and symbols.');
+      return false;
+    }
+  
+    // All checks passed, data is valid
+    return true;
+  }
+
+
 }
