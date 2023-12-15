4b42b2386950030fcad6ddad862262239ae4a834
Cw020729 <lchowwai@gmail.com>
2023-12-15 17:11:27 +0800

    Added data validation for add item not include image

--- src/app/merchantadditem/merchantadditem.component.ts
+++ src/app/merchantadditem/merchantadditem.component.ts
@@ -27,19 +27,57 @@ export class MerchantadditemComponent {
     };
 
     const merchantEmail = this.authService.getmerchantEmail(); // Replace with the actual email
+    if(this.isFormDataValid()){
+      this.http.post('http://localhost:8080/merchant/add-item', foodItem, {
+        params: { merchantEmail },
+      })
+        .subscribe(
+          (response) => {
+            console.log('Success:', response);
+            alert("You have succesfully added the Item")
+            // Handle success (e.g., show a success message)
+          },
+          (error) => {
+            console.error('Error:', error);
+            // Handle error (e.g., show an error message)
+          }
+        );
+    }
+  }
+  isFormDataValid(): boolean {
+     
+    if (!this.itemName.trim()) {
+      alert('Please enter Item Name');
+      return false;
+    }else if (!/^[a-zA-Z ]+$/.test(this.itemName.trim())){
+      alert('Item Name should only contain letter') ;
+      return false;
+    }
 
-    this.http.post('http://localhost:8080/merchant/add-item', foodItem, {
-      params: { merchantEmail },
-    })
-      .subscribe(
-        (response) => {
-          console.log('Success:', response);
-          // Handle success (e.g., show a success message)
-        },
-        (error) => {
-          console.error('Error:', error);
-          // Handle error (e.g., show an error message)
-        }
-      );
+    if (!this.itemDescription.trim()) {
+      alert('Please enter Item Description');
+      return false;
+    }
+
+    if (!this.itemPrice) {
+      alert('Please enter Item Price');
+      return false;
+    }else if (this.itemPrice > 1000){
+      alert('The price should below RYM 1000')
+      return false;
+    }else if (!/^\d+(,\d{1,2})?$/.test(this.itemPrice.toString())){
+      alert('Please enter an valid price')
+      return false;
+    }
+
+    if (!this.itemCategory.trim()) {
+      alert('Please enter Item Category');
+      return false;
+    }else if (!/^[a-zA-Z ]+$/.test(this.itemCategory.trim())){
+      alert('Item Category should only contain letter') ;
+      return false;
+    }
+    // All checks passed, data is valid
+    return true;
   }
 }
