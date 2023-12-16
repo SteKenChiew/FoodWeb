9a38d611c8b8022784ad5215ed529d24504daf48
Ste Ken <chiewsteken@gmail.com>
2023-12-16 13:57:14 +0800

    Realtime order update

--- src/app/merchantadditem/merchantadditem.component.ts
+++ src/app/merchantadditem/merchantadditem.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {MerchantauthService} from '../services/merchantauth.service';
+import { MerchantauthService } from '../services/merchantauth.service';
 
 @Component({
   selector: 'app-merchantadditem',
@@ -11,35 +11,37 @@ export class MerchantadditemComponent {
   itemName: string = '';
   itemDescription: string = '';
   itemPrice: number = 0;
-  itemImg: String = '';
-  itemCategory: String ='';
+  itemImg: File | null = null;
+  itemCategory: string = '';
 
   constructor(private http: HttpClient, private authService: MerchantauthService) {}
 
+  onFileSelected(event: any) {
+    this.itemImg = event.target.files[0] as File;
+  }
+
   addItem() {
-    const foodItem = {
+    const formData = new FormData();
+    formData.append('image', this.itemImg as Blob, this.itemImg?.name || '');
+    formData.append('merchantEmail', this.authService.getmerchantEmail());
+    formData.append('foodItem', JSON.stringify({
       itemName: this.itemName,
-      itemImg: this.itemImg,
       itemDescription: this.itemDescription,
       itemPrice: this.itemPrice,
       itemCategory: this.itemCategory
-      // Add other properties as needed
-    };
-
-    const merchantEmail = this.authService.getmerchantEmail(); // Replace with the actual email
-
-    this.http.post('http://localhost:8080/merchant/add-item', foodItem, {
-      params: { merchantEmail },
-    })
+    }));
+  
+    // Remove Firebase-specific code
+  
+    this.http.post('http://localhost:8080/merchant/add-item', formData)
       .subscribe(
         (response) => {
           console.log('Success:', response);
-          // Handle success (e.g., show a success message)
         },
         (error) => {
           console.error('Error:', error);
-          // Handle error (e.g., show an error message)
         }
       );
   }
+  
 }
