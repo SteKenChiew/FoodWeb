e2e40a8fea6490faffab4514c2eede445bb58413
Ste Ken <chiewsteken@gmail.com>
2023-12-18 14:56:01 +0800

    Admin login

--- src/app/adminlogin/adminlogin.component.ts
+++ src/app/adminlogin/adminlogin.component.ts
@@ -1,4 +1,7 @@
+// adminlogin.component.ts
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-adminlogin',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./adminlogin.component.css']
 })
 export class AdminloginComponent {
+  email: string = '';
+  password: string = '';
+  
+  constructor(private http: HttpClient, private router: Router) {}
 
+  login(): void {
+    this.http.post('http://localhost:8080/admin/login', { email: this.email, password: this.password })
+      .subscribe(
+        (response) => {
+          this.router.navigate(['adminmainpage']);
+          console.log(response);
+        },
+        (error) => {
+          // Handle error response
+          if (error instanceof HttpErrorResponse) {
+            console.error(`Backend returned code ${error.status}, body was: ${error.error}`);
+          } else {
+            console.error(`An unknown error occurred: ${error}`);
+          }
+        }
+      );
+  }
+  
 }
